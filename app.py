f1f711c8c6f22be727ee386ac9f805248eb123e5
cloudpunchx <siobhanbonardi@hotmail.com>
2023-01-27 00:52:17 -0700

    Finished the 2nd part of the bonus, added Try and Exception with while loop

--- app.py
+++ app.py
@@ -10,29 +10,50 @@ print("3. Multiplication")
 print("4. Division")
 selection = input("Please enter your selection: ")
 selection = int(selection)
-if (selection == 1):
-    x = input("Type first number: ")
-    y = input("Type second number: ")
-    x_num = int(x)
-    y_num = int(y)
-    add.add(x_num, y_num)
-elif (selection == 2):
-    x = input("Type first number: ")
-    y = input("Type second number: ")
-    x_num = int(x)
-    y_num = int(y)
-    subtract.subtract(x_num, y_num)
-elif (selection == 3):
-    x = input("Type first number: ")
-    y = input("Type second number: ")
-    x_num = int(x)
-    y_num = int(y)
-    multiply.multiply(x_num, y_num)
-elif (selection == 4):
-    x = input("Type first number: ")
-    y = input("Type second number: ")
-    x_num = int(x)
-    y_num = int(y)
-    divide.divide(x_num, y_num)
-else:
-    print("Please make a selection from 1-4")
+is_valid = False
+while not is_valid:
+    if (selection == 1):
+        x = input("Type first number: ")
+        y = input("Type second number: ")
+        try: 
+            x_num = int(x)
+            y_num = int(y)
+            add.add(x_num, y_num)
+            is_valid = True
+        except ValueError:
+            print("Invalid character entered.")
+    elif (selection == 2):
+        x = input("Type first number: ")
+        y = input("Type second number: ")
+        try: 
+            x_num = int(x)
+            y_num = int(y)
+            subtract.subtract(x_num, y_num)
+            is_valid = True
+        except ValueError:
+            print("Invalid character entered.")
+    elif (selection == 3):
+        x = input("Type first number: ")
+        y = input("Type second number: ")
+        try: 
+            x_num = int(x)
+            y_num = int(y)
+            multiply.multiply(x_num, y_num)
+            is_valid = True
+        except ValueError:
+            print("Invalid character entered.")
+    elif (selection == 4):
+        x = input("Type first number: ")
+        y = input("Type second number: ")
+        try: 
+            x_num = int(x)
+            y_num = int(y)
+            divide.divide(x_num, y_num)
+            is_valid = True
+        except ValueError:
+            print("Invalid character entered.")
+    else:
+        print("Please make a selection from 1-4")
+
+#  Create a custom exception called CalculatorInputError to be thrown 
+#  if your user inputs any invalid characters. The calculator should ask for the input again if this error is thrown.
